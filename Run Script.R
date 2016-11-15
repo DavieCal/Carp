d44e2e86b0b70ea58debf7809b5b939a37707e77
DavieCal <daviecalmail@gmail.com>
2016-11-15 11:58:09 +0000

    update nov 14

--- Run Script.R
+++ Run Script.R
@@ -1,5 +1,5 @@
 #Install required packages
-packages <- c('lubridate','pscl','car','lmtest','ZIM','sandwhich')
+packages <- c('lubridate','pscl','car','lmtest','ZIM','sandwhich','texreg')
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
   install.packages(setdiff(packages, rownames(installed.packages())))  
 }
